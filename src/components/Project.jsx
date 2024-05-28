3f542b400a2d7835b164e21a53bf8d641f0cdc1a
SanketBhandarii <bhandarisanketp@gmail.com>
2024-05-28 19:11:44 +0530

    Updated

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,19 @@ import zerodha from "../assets/zerodha.png";
 import bucketlist from "../assets/bucketlist.png";
 import currency from "../assets/currency.png";
 import taxcal from "../assets/taxcal.png";
+import { Tilt } from "react-tilt";
+
+const defaultOptions = {
+  reverse: false,
+  max: 35,
+  perspective: 1000,
+  scale: 1,
+  speed: 1000,
+  transition: true,
+  axis: null,
+  reset: true,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+};
 
 function Project() {
   const [dark] = useContext(DarkMode);
@@ -30,218 +43,226 @@ function Project() {
       </p>
 
       <main className="flex flex-wrap gap-10 justify-center mt-4 px-5">
-        <div
-          className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer hover:scale-105 transition-all duration-300 ${
-            dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
-          }`}
-        >
-          <img
-            src={zerodha}
-            alt="Zerodha UI"
-            className="w-full h-48 object-cover"
-          />
-          <div className="px-5 pt-9">
-            <h1
-              className={`text-center text-2xl ${
-                dark ? "text-gray-300" : "text-black"
-              }`}
-            >
-              Zerodha <span className="text-cyan-700">UI</span>
-            </h1>
-            <p
-              className={`text-base mt-3 h-24 ${
-                dark ? "text-gray-400" : "text-black"
-              }`}
-            >
-              Zerodha UI crafted with Tailwind CSS: clean, responsive, and
-              modern.
-            </p>
-            <div className="flex justify-around mt-6">
-              <a
-                className={`text-sm flex gap-2 items-center ${
-                  dark ? "text-stone-300" : "text-cyan-700"
+        <Tilt options={defaultOptions}>
+          <div
+            className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer transition-all duration-300 ${
+              dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
+            }`}
+          >
+            <img
+              src={zerodha}
+              alt="Zerodha UI"
+              className="w-full h-48 object-cover"
+            />
+            <div className="px-5 pt-9">
+              <h1
+                className={`text-center text-2xl ${
+                  dark ? "text-gray-300" : "text-black"
                 }`}
-                href="https://zeroodha.netlify.app/"
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                <i className="fa-solid fa-paperclip"></i>
-                Live Preview
-              </a>
-              <a
-                className={`text-sm flex gap-2 items-center ${
-                  dark ? "text-white" : "text-black"
+                Zerodha <span className="text-cyan-700">UI</span>
+              </h1>
+              <p
+                className={`text-base mt-3 h-24 ${
+                  dark ? "text-gray-400" : "text-black"
                 }`}
-                href="https://github.com/SanketBhandarii/Zerodha"
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                <i className="fa-solid fa-code"></i>
-                View Code
-              </a>
+                Zerodha UI crafted with Tailwind CSS: clean, responsive, and
+                modern.
+              </p>
+              <div className="flex justify-around mt-6">
+                <a
+                  className={`text-sm flex gap-2 items-center ${
+                    dark ? "text-stone-300" : "text-cyan-700"
+                  }`}
+                  href="https://zeroodha.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-paperclip"></i>
+                  Live Preview
+                </a>
+                <a
+                  className={`text-sm flex gap-2 items-center ${
+                    dark ? "text-white" : "text-black"
+                  }`}
+                  href="https://github.com/SanketBhandarii/Zerodha"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-code"></i>
+                  View Code
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Tilt>
 
-        <div
-          className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer hover:scale-105 transition-all duration-300 ${
-            dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
-          }`}
-        >
-          <img
-            src={bucketlist}
-            alt="Bucket List App"
-            className="w-full h-48 object-cover"
-          />
-          <div className="px-5 pt-9">
-            <h1
-              className={`text-center text-2xl ${
-                dark ? "text-gray-300" : "text-black"
-              }`}
-            >
-              Bucket <span className="text-cyan-700">List</span>
-            </h1>
-            <p
-              className={`text-base mt-3 h-24 ${
-                dark ? "text-gray-400" : "text-black"
-              }`}
-            >
-              Bucket List app built with React JS: organized, dynamic, and
-              user-friendly.
-            </p>
-            <div className="flex justify-around mt-6">
-              <a
-                className={`text-sm flex gap-2 items-center ${
-                  dark ? "text-stone-300" : "text-cyan-700"
+        <Tilt options={defaultOptions}>
+          <div
+            className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer transition-all duration-300 ${
+              dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
+            }`}
+          >
+            <img
+              src={bucketlist}
+              alt="Bucket List App"
+              className="w-full h-48 object-cover"
+            />
+            <div className="px-5 pt-9">
+              <h1
+                className={`text-center text-2xl ${
+                  dark ? "text-gray-300" : "text-black"
                 }`}
-                href="https://bucket-listapp.netlify.app/"
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                <i className="fa-solid fa-paperclip"></i>
-                Live Preview
-              </a>
-              <a
-                className={`text-sm flex gap-2 items-center ${
-                  dark ? "text-white" : "text-black"
+                Bucket <span className="text-cyan-700">List</span>
+              </h1>
+              <p
+                className={`text-base mt-3 h-24 ${
+                  dark ? "text-gray-400" : "text-black"
                 }`}
-                href="https://github.com/SanketBhandarii/BucketListApp"
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                <i className="fa-solid fa-code"></i>
-                View Code
-              </a>
+                Bucket List app built with React JS: organized, dynamic, and
+                user-friendly.
+              </p>
+              <div className="flex justify-around mt-6">
+                <a
+                  className={`text-sm flex gap-2 items-center ${
+                    dark ? "text-stone-300" : "text-cyan-700"
+                  }`}
+                  href="https://bucket-listapp.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-paperclip"></i>
+                  Live Preview
+                </a>
+                <a
+                  className={`text-sm flex gap-2 items-center ${
+                    dark ? "text-white" : "text-black"
+                  }`}
+                  href="https://github.com/SanketBhandarii/BucketListApp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-code"></i>
+                  View Code
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Tilt>
 
-        <div
-          className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer hover:scale-105 transition-all duration-300 ${
-            dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
-          }`}
-        >
-          <img
-            src={currency}
-            alt="Currency Converter"
-            className="w-full h-48 object-cover"
-          />
-          <div className="px-5 pt-9">
-            <h1
-              className={`text-center text-2xl ${
-                dark ? "text-gray-300" : "text-black"
-              }`}
-            >
-              Currency <span className="text-cyan-700">Converter</span>
-            </h1>
-            <p
-              className={`text-base mt-3 h-24 ${
-                dark ? "text-gray-400" : "text-black"
-              }`}
-            >
-              Currency Converter project: HTML, CSS, JS - intuitive and
-              functional.
-            </p>
-            <div className="flex justify-around mt-6">
-              <a
-                className={`text-sm flex gap-2 items-center ${
-                  dark ? "text-stone-300" : "text-cyan-700"
+        <Tilt options={defaultOptions}>
+          <div
+            className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer transition-all duration-300 ${
+              dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
+            }`}
+          >
+            <img
+              src={currency}
+              alt="Currency Converter"
+              className="w-full h-48 object-cover"
+            />
+            <div className="px-5 pt-9">
+              <h1
+                className={`text-center text-2xl ${
+                  dark ? "text-gray-300" : "text-black"
                 }`}
-                href="https://currenccyconverter.netlify.app/"
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                <i className="fa-solid fa-paperclip"></i>
-                Live Preview
-              </a>
-              <a
-                className={`text-sm flex gap-2 items-center ${
-                  dark ? "text-white" : "text-black"
+                Currency <span className="text-cyan-700">Converter</span>
+              </h1>
+              <p
+                className={`text-base mt-3 h-24 ${
+                  dark ? "text-gray-400" : "text-black"
                 }`}
-                href="https://github.com/SanketBhandarii/Currency-Converter"
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                <i className="fa-solid fa-code"></i>
-                View Code
-              </a>
+                Currency Converter project: HTML, CSS, JS - intuitive and
+                functional.
+              </p>
+              <div className="flex justify-around mt-6">
+                <a
+                  className={`text-sm flex gap-2 items-center ${
+                    dark ? "text-stone-300" : "text-cyan-700"
+                  }`}
+                  href="https://currenccyconverter.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-paperclip"></i>
+                  Live Preview
+                </a>
+                <a
+                  className={`text-sm flex gap-2 items-center ${
+                    dark ? "text-white" : "text-black"
+                  }`}
+                  href="https://github.com/SanketBhandarii/Currency-Converter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-code"></i>
+                  View Code
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Tilt>
 
-        <div
-          className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer hover:scale-105 transition-all duration-300 ${
-            dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
-          }`}
-        >
-          <img
-            src={taxcal}
-            alt="Tax Calculator"
-            className="w-full h-48 object-cover"
-          />
-          <div className="px-5 pt-9">
-            <h1
-              className={`text-center text-2xl ${
-                dark ? "text-gray-300" : "text-black"
-              }`}
-            >
-              Tax <span className="text-cyan-700">Calculator</span>
-            </h1>
+        <Tilt options={defaultOptions}>
+          <div
+            className={`w-72 rounded-lg overflow-hidden h-auto pb-10 cursor-pointer transition-all duration-300 ${
+              dark ? "bg-bgClr_2" : "bg-white shadow-2xl shadow-slate-300"
+            }`}
+          >
+            <img
+              src={taxcal}
+              alt="Tax Calculator"
+              className="w-full h-48 object-cover"
+            />
+            <div className="px-5 pt-9">
+              <h1
+                className={`text-center text-2xl ${
+                  dark ? "text-gray-300" : "text-black"
+                }`}
+              >
+                Tax <span className="text-cyan-700">Calculator</span>
+              </h1>
 
-            <p
-              className={`text-base ${
-                dark ? "text-gray-400" : "text-black"
-              } mt-3 h-24`}
-            >
-              Tax Calculator: Calculates tax based on annual income and
-              applicable rates
-            </p>
-            <div className="flex justify-around mt-6">
-              <a
-                className={` text-sm ${
-                  dark ? "text-stone-300" : "text-cyan"
-                } flex gap-2 items-center`}
-                href="https://taxcalculatorr.netlify.app/"
-                target="_blank"
-                rel="noopener noreferrer"
+              <p
+                className={`text-base ${
+                  dark ? "text-gray-400" : "text-black"
+                } mt-3 h-24`}
               >
-                <i className="fa-solid fa-paperclip"></i>
-                Live Preview
-              </a>
-              <a
-                className={` text-sm ${
-                  dark ? "text-white" : "text-black"
-                } flex gap-2 items-center`}
-                href="https://github.com/SanketBhandarii/Income-Tax-Calculator"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fa-solid fa-code"></i>
-                View Code
-              </a>
+                Tax Calculator: Calculates tax based on annual income and
+                applicable rates
+              </p>
+              <div className="flex justify-around mt-6">
+                <a
+                  className={` text-sm ${
+                    dark ? "text-stone-300" : "text-cyan"
+                  } flex gap-2 items-center`}
+                  href="https://taxcalculatorr.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-paperclip"></i>
+                  Live Preview
+                </a>
+                <a
+                  className={` text-sm ${
+                    dark ? "text-white" : "text-black"
+                  } flex gap-2 items-center`}
+                  href="https://github.com/SanketBhandarii/Income-Tax-Calculator"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-solid fa-code"></i>
+                  View Code
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Tilt>
       </main>
     </section>
   );
