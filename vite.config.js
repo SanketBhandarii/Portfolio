f0d7b7da0374019246b98a1a62e03f6897f72bf7
SanketBhandarii <bhandarisanketp@gmail.com>
2024-10-10 17:09:06 +0530

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio/',  // Add your repository name here
+  plugins: [react()],  // Add your repository name here
 });
