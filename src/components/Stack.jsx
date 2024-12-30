d8ec957ef4ffc1cdded0d1cf7db4b4e8dadf050c
SanketBhandarii <bhandarisanketp@gmail.com>
2024-12-30 12:11:03 +0530

    Updates

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -16,13 +16,18 @@ function Stack() {
       <p className="text-xl text-center dark:text-gray-400 text-black max-md:px-3">
         Technologies I've been working with recently
       </p>
-      <div className="flex gap-16 flex-wrap mt-20 justify-center items-center px-10">
-        <i className="fa-brands fa-html5 text-orange-500 text-8xl"></i>
-        <i className="fa-brands fa-css3-alt text-sky-600 text-8xl"></i>
+      <div className="flex gap-14 max-sm:gap-11 flex-wrap mt-20 justify-center items-center px-10">
         <i className="fa-brands fa-js text-yellow-300 text-8xl"></i>
+        <img
+          src="https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo-thumbnail.png"
+          className="rounded-full"
+          width="100px"
+        />
+        <i className="fa-brands fa-css3-alt text-sky-600 text-8xl"></i>
+
         <i className="fa-brands fa-react text-cyan-400 text-8xl"></i>
         <img src={tailwind} width="100px" />
-        <img src={sql} width="70px" />
+        <img src="https://yt3.googleusercontent.com/1oQc-j55vr_tnNhIWvSTxSPeV9cPpZyC3IoTr4zl6oUvEK50z9PjtfvKfyL8qC-sNbcQQmYg=s900-c-k-c0x00ffffff-no-rj" width="100px" className="rounded-full" />
         <img src={mongo} width="100px" className="ml-0" />
         <img src={java} width="100px" />
       </div>
